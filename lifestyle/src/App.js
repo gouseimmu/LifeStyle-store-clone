23d12d7bb8ef86f51dd5652c3c8c63685fa3c0c6
madhavi-teegireddy <madhavitigireddi@gmail.com>
2022-12-14 12:38:56 +0530

    women

--- lifestyle/src/App.js
+++ lifestyle/src/App.js
@@ -4,10 +4,12 @@ import MainRoutes from "./pages/MainRoutes";
 
 // import MainRoutes from './pages/MainRoutes';
 import Navbar from './components/Navbar';
+import Women from "./pages/products/Women"
 
 function App() {
   return (
     <div className="App">
+      <Women/>
     </div>
   );
 }
