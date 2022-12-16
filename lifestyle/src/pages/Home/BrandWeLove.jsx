4f15001c314444f4b142e31acdade2a07f3bc4c2
arpitkumarsahu <aarpitkumarsahu99@gmail.com>
2022-12-16 11:35:06 +0530

    Home Page added

---
+++ lifestyle/src/pages/Home/BrandWeLove.jsx
@@ -0,0 +1,68 @@
+import { Divider, Grid, GridItem, Heading, Image } from '@chakra-ui/react'
+import React from 'react'
+
+function BrandWeLove() {
+  return (
+    <div>
+        <Heading
+        fontFamily={
+          'ProximaNova-Regular,"Helvetica Neue",Arial,sans-serif,"Apple Color Emoji","Segoe UI Emoji"'
+        }
+        fontSize={"30px"}
+        fontWeight={"600"}
+        size="xl"
+        mt={"20px"}
+        textAlign={"left"}
+      >
+        Brands We Love
+      </Heading>
+      <Divider
+        bg={"rgb(250, 166, 25)"}
+        borderColor={"rgb(250, 166, 25)"}
+        w={"60px"}
+        mb={"20px"}
+        borderWidth={"2px"}
+      />
+      <Grid templateColumns='repeat(6, 1fr)' gap={6} >
+        <GridItem>
+            <Image src={'https://lmsin.net/cdn-cgi/image/w=616,q=70,fit=cover/https://70415bb9924dca896de0-34a37044c62e41b40b39fcedad8af927.lmsin.net/LS-Fest/LS-new/LSUber-Desktop-Promowidget19-Banner1-29Nov22.jpg'}/>
+        </GridItem>
+        <GridItem>
+            <Image src={'https://lmsin.net/cdn-cgi/image/w=616,q=70,fit=cover/https://70415bb9924dca896de0-34a37044c62e41b40b39fcedad8af927.lmsin.net/LS-Fest/LS-new/LSUber-Desktop-Promowidget19-Banner2-29Nov22.jpg'}/>
+        </GridItem>
+        <GridItem>
+            <Image src={'https://lmsin.net/cdn-cgi/image/w=616,q=70,fit=cover/https://70415bb9924dca896de0-34a37044c62e41b40b39fcedad8af927.lmsin.net/LS-Fest/LS-new/LSUber-Desktop-Promowidget19-Banner3-29Nov22.jpg'}/>
+        </GridItem>
+        <GridItem>
+            <Image src={'https://lmsin.net/cdn-cgi/image/w=616,q=70,fit=cover/https://70415bb9924dca896de0-34a37044c62e41b40b39fcedad8af927.lmsin.net/LS-Fest/LS-new/LSUber-Desktop-Promowidget19-Banner4-15Dec22.jpg'}/>
+        </GridItem>
+        <GridItem>
+            <Image src={'https://lmsin.net/cdn-cgi/image/w=616,q=70,fit=cover/https://70415bb9924dca896de0-34a37044c62e41b40b39fcedad8af927.lmsin.net/LS-Fest/LS-new/LSUber-Desktop-Promowidget19-Banner5-29Nov22.jpg'}/>
+        </GridItem>
+        <GridItem>
+            <Image src={'https://lmsin.net/cdn-cgi/image/w=616,q=70,fit=cover/https://70415bb9924dca896de0-34a37044c62e41b40b39fcedad8af927.lmsin.net/LS-Fest/LS-new/LSUber-Desktop-Promowidget19-Banner6-29Nov22.jpg'}/>
+        </GridItem>
+        <GridItem>
+            <Image src={'https://lmsin.net/cdn-cgi/image/w=616,q=70,fit=cover/https://70415bb9924dca896de0-34a37044c62e41b40b39fcedad8af927.lmsin.net/LS-Fest/LS-new/LSUber-Desktop-Promowidget19-Banner7-15Dec22A.jpg'}/>
+        </GridItem>
+        <GridItem>
+            <Image src={'https://lmsin.net/cdn-cgi/image/w=616,q=70,fit=cover/https://70415bb9924dca896de0-34a37044c62e41b40b39fcedad8af927.lmsin.net/LS-Fest/LS-new/LSUber-Desktop-Promowidget19-Banner8-15Dec22.jpg'}/>
+        </GridItem>
+        <GridItem>
+            <Image src={'https://lmsin.net/cdn-cgi/image/w=616,q=70,fit=cover/https://70415bb9924dca896de0-34a37044c62e41b40b39fcedad8af927.lmsin.net/LS-Fest/LS-new/LSUber-Desktop-Promowidget19-Banner9-29Nov22.jpg'}/>
+        </GridItem>
+        <GridItem>
+            <Image src={'https://lmsin.net/cdn-cgi/image/w=616,q=70,fit=cover/https://70415bb9924dca896de0-34a37044c62e41b40b39fcedad8af927.lmsin.net/LS-Fest/LS-new/LSUber-Desktop-Promowidget19-Banner10-15Dec22.jpg'}/>
+        </GridItem>
+        <GridItem>
+            <Image src={'https://lmsin.net/cdn-cgi/image/w=616,q=70,fit=cover/https://70415bb9924dca896de0-34a37044c62e41b40b39fcedad8af927.lmsin.net/LS-Fest/LS-new/LSUber-Desktop-Promowidget19-Banner11-15Dec22.jpg'}/>
+        </GridItem>
+        <GridItem>
+            <Image src={'https://lmsin.net/cdn-cgi/image/w=616,q=70,fit=cover/https://70415bb9924dca896de0-34a37044c62e41b40b39fcedad8af927.lmsin.net/LS-Fest/LS-new/LSUber-Desktop-Promowidget19-Banner12-29Nov22.jpg'}/>
+        </GridItem>
+      </Grid>
+    </div>
+  )
+}
+
+export default BrandWeLove
